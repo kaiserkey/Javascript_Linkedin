11f3e4e50a0004d0039cdcce290bbfc3d160ad84
kaiserkey <kaiserkey2@gmail.com>
2023-08-26 22:21:36 -0300

    new changes

--- ejercicio04.js
+++ ejercicio04.js
@@ -120,3 +120,16 @@ console.log(z);
 z = a !== b;
 console.log(z);
 
+
+var a = 1;
+var b = 2;
+var c = 5;
+
+var op1 = a < b;
+var op2 = c <= a;
+var op3 = (a > b) != (c > a) ;
+console.log("Op1: " + op1);
+console.log("Op2: " + op2);
+console.log("Op3: " + op3);
+
+
