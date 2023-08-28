89c646e1b048f33cc108cfa6363d775f51156e44
kaiserkey <kaiserkey2@gmail.com>
2023-08-28 19:28:11 -0300

    new changes

--- ejercicio05.js
+++ ejercicio05.js
@@ -39,18 +39,14 @@ switch (numero) {
         console.log("No soy nada");
 }
 
-var a = 12
-var b = 4
-var c = 5
+var a = 5
+var b = 12
+var c = 4
 
-if (a>b) {
-    if (b<c) {
-       console.log("El valor de b es: " + b);
-    }
-    else if (b>c) {
-       console.log("El valor de c es: " + c);
-    }
- } 
- else {
-    console.log("El valor de a es: " + a );
- }
+if ((b > c) && !(c < a)) {
+    console.log('Se ejecutó la primera condición');
+} else if (!(b >= a) && (c > !b)) {
+    console.log('Se ejecutó la segunda condición');
+} else {
+    console.log('Se ejecutó el último else')
+}
