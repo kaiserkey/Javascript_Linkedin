712f3b09df8ae6c6154df69a4c9cf44a6cf9bae8
kaiserkey <kaiserkey2@gmail.com>
2023-08-28 19:15:25 -0300

    new changes

--- ejercicio05.js
+++ ejercicio05.js
@@ -16,11 +16,6 @@ if (edad === 18) {
 var edad = 18;
 if (edad === 18) {
     console.log("Puedes votar, será tu primera votación");
-    
-} else if (edad > 18) {
-    console.log("Puedes votar de nuevo");
-} else {
-    console.log("Aún no puedes votar");
 }
 
 //operador ternario
