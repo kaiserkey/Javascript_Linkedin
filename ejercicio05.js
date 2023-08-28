2cf2dc39918c83c0cb8640c13b3574c58584d8b7
kaiserkey <kaiserkey2@gmail.com>
2023-08-28 19:07:36 -0300

    new changes

--- ejercicio05.js
+++ ejercicio05.js
@@ -1,3 +1,10 @@
 "strict mode"
 
 //condicionales
+var edad = 18;
+
+if (edad === 18) {
+    console.log("Puedes votar, será tu primera votación");
+} else if (edad > 18) {
+    console.log("Puedes votar de nuevo");
+} else {
