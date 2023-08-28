a2f390aaecfb1ff99d32d7fbd5a7dcbe83d40558
kaiserkey <kaiserkey2@gmail.com>
2023-08-28 20:45:52 -0300

    new changes

--- ejercicio07.js
+++ ejercicio07.js
@@ -89,7 +89,7 @@ const btnSaludar = document.getElementById("btnSaludar");
 
 btnSaludar.addEventListener("click", function(){
     console.log("Saludando");
-    this.loca
+    this.location.href = "https://www.google.com";
 });
 
 //funciones como objetos
