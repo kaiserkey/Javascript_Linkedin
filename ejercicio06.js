76e742a8af94fd31c5fe4a5f773336d6b6b20579
kaiserkey <kaiserkey2@gmail.com>
2023-08-28 19:43:47 -0300

    new changes

--- ejercicio06.js
+++ ejercicio06.js
@@ -67,7 +67,8 @@ colores.forEach((color, index) => console.log(`${index} - ${color}`)); // se pue
 
 //ciclo for each (nuevo en ES6) se usa para recorrer arreglos y objetos iterables
 
-colores.forEach((color, index, arreglo) => console.log(`${index} - ${color} - ${arreglo}`)); // se puede usar una funcion anonima o una funcion flecha, el a
+colores.forEach((color, index, arreglo) => console.log(`${index} - ${color} - ${arreglo}`)); // se puede usar una funcion anonima o una funcion flecha, el arreglo es opcional y es el arreglo que se esta recorriendo
+
 
 
 
