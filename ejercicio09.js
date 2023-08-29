8bf08a5d6ff7493c69454e4596e4e53b24ad60ae
kaiserkey <kaiserkey2@gmail.com>
2023-08-29 19:48:54 -0300

    new changes

--- ejercicio09.js
+++ ejercicio09.js
@@ -1,11 +1,11 @@
 "use strict"
 
-//setInterval
 function setColor(){
     let pagina = document.body;
     pagina.style.backgroundColor = pagina.style.backgroundColor == "red" ? "green" : "red";
 }
 
+//setInterval sirve para ejecutar una función cada cierto tiempo
 var temporizador = setInterval(setColor, 2000);
 
 function stopChangeColor(){}
