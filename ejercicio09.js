246f28e647a8c1bea2a7a6a64883b6e64b2c07cf
kaiserkey <kaiserkey2@gmail.com>
2023-08-29 19:49:49 -0300

    new changes

--- ejercicio09.js
+++ ejercicio09.js
@@ -5,8 +5,12 @@ function setColor(){
     pagina.style.backgroundColor = pagina.style.backgroundColor == "red" ? "green" : "red";
 }
 
+function stopChangeColor(){
+    clearInterval(temporizador);
+    clearTimeout(temporizador2);
+}
+
 //setInterval sirve para ejecutar una función cada cierto tiempo
 var temporizador = setInterval(setColor, 2000);
 //setTimeout sirve para ejecutar una función una sola vez después de cierto tiempo
-var temporizador2 = setTimeout(stopChangeColor, 10000);
-function stopChangeColor(){}
+var temporizador2 = setTimeout(stopChangeColor, 6000);
