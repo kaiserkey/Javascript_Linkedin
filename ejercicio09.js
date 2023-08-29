6d07b7f95aba6a2e10c44dec449c6446f8341e95
kaiserkey <kaiserkey2@gmail.com>
2023-08-29 19:49:04 -0300

    new changes

--- ejercicio09.js
+++ ejercicio09.js
@@ -7,5 +7,5 @@ function setColor(){
 
 //setInterval sirve para ejecutar una función cada cierto tiempo
 var temporizador = setInterval(setColor, 2000);
-
+var temporizador
 function stopChangeColor(){}
