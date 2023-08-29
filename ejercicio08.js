ec51e64984ca2a167787ece44ea26ce1266763fb
kaiserkey <kaiserkey2@gmail.com>
2023-08-29 19:22:52 -0300

    new changes

--- ejercicio08.js
+++ ejercicio08.js
@@ -8,13 +8,17 @@ boton.addEventListener("click", ()=>{
     console.log("Has pulsado el botón");
 });
 
+//evento mouseover
 boton.addEventListener("mouseover", ()=> {
     console.log("Has pasado el ratón por encima");
 });
 
+//evento mouseout
 boton.addEventListener("mouseout", ()=> {
     console.log("Has salido del botón");
 });
+
+
 
 
 
