4e1dcf9ab22b578f32156d08dbf4516bba0a3862
kaiserkey <kaiserkey2@gmail.com>
2023-08-29 19:19:27 -0300

    new changes

--- ejercicio08.js
+++ ejercicio08.js
@@ -7,3 +7,6 @@ boton.addEventListener("click", function(){
     alert("Has pulsado el botón");
 });
 
+boton.addEventListener("mouseover", function(){
+    alert("Has pasado el ratón por encima");
+});
