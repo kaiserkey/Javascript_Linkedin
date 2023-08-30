f6d2e2fa91ee0104bfcdc94efa05d2b4485cfaf8
kaiserkey <kaiserkey2@gmail.com>
2023-08-30 19:54:38 -0300

    new changes

--- ejercicio10.js
+++ ejercicio10.js
@@ -11,4 +11,5 @@ var boton2 = document.getElementsByTagName("button")[1];
 boton2.addEventListener("click", ()=>{ 
     let valor = confirm("Esto es una ventana de confirmación");
     console.log(valor);
+    if 
 });
