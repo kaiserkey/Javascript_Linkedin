d7d2b1f7b53caa02b26120dace49270c527bda6c
kaiserkey <kaiserkey2@gmail.com>
2023-08-30 19:51:09 -0300

    new changes

--- ejercicio10.js
+++ ejercicio10.js
@@ -6,5 +6,4 @@ boton.addEventListener("click", ()=>{
     var ventana = window.open("https://www.google.com", "Ventana emergente", "width=400, height=300");
     ventana.moveTo(100, 100);
     ventana.resizeTo(500, 500);
-    ventana.close();
 });
