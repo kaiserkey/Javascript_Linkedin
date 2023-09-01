2c3b1735f4c341bc652bc56234413edd5e928ec7
kaiserkey <kaiserkey2@gmail.com>
2023-08-31 22:12:40 -0300

    new changes

--- ejercicio11.js
+++ ejercicio11.js
@@ -7,3 +7,5 @@ console.log("MIN_VALUE: " + Number.MIN_VALUE);
 console.log("NEGATIVE_INFINITY: " + Number.NEGATIVE_INFINITY);
 console.log("POSITIVE_INFINITY: " + Number.POSITIVE_INFINITY);
 console.log("NaN: " + Number.NaN);
+
+// me
