33f67dc48561bd731c046def5c420a77e51640ad
kaiserkey <kaiserkey2@gmail.com>
2023-08-31 22:17:12 -0300

    new changes

--- ejercicio11.js
+++ ejercicio11.js
@@ -11,14 +11,14 @@ console.log("NaN: " + Number.NaN);
 // metodos para trabajar con numeros
 
 var numero = "10.5";
-var 
+var numero2 = 10.5;
 console.log("parseInt: " + parseInt(numero)); // convierte a entero
 console.log("parseFloat: " + parseFloat(numero)); // convierte a flotante
 console.log("isNaN: " + isNaN(numero)); // comprueba si es un numero
 console.log("isInteger: " + Number.isInteger (numero)); // comprueba si es un entero
 console.log("isSafeInteger: " + Number.isSafeInteger(numero)); // comprueba si es un entero seguro (no se desborda) 
-console.log("toExponential: " + numero.toExponential(4)); // convierte a notacion exponencial con 4 decimales
-console.log("toFixed: " + numero.toFixed(4)); // convierte a notacion fija con 4 decimales
-console.log("toPrecision: " + numero.toPrecision(4)); // convierte a notacion fija con 4 decimales
+console.log("toExponential: " + numero2.toExponential(4)); // convierte a notacion exponencial con 4 decimales
+console.log("toFixed: " + numero2.toFixed(4)); // convierte a notacion fija con 4 decimales
+console.log("toPrecision: " + numero2.toPrecision(4)); // convierte a notacion fija con 4 decimales
 console.log("toString: " + numero.toString()); // convierte a cadena
 console.log("valueOf: " + numero.valueOf()); // devuelve el valor primitivo del objeto
