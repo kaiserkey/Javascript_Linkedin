fc4590636afc5dc21e028635228231859a08b74c
kaiserkey <kaiserkey2@gmail.com>
2023-08-31 22:12:18 -0300

    new changes

--- ejercicio11.js
+++ ejercicio11.js
@@ -3,3 +3,5 @@
 // propiedades numericas
 
 console.log("MAX_VALUE: " + Number.MAX_VALUE);
+console.log("MIN_VALUE: " + Number.MIN_VALUE);
+console.log("NEGATIVE_INFINITY: " + Number.NEGATIVE_INFINITY);
