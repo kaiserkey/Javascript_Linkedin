4121954fc579270fe8356fa14172fda3731ac1b9
kaiserkey <kaiserkey2@gmail.com>
2023-09-03 20:39:02 -0300

    new changes

--- ejercicio12.js
+++ ejercicio12.js
@@ -93,7 +93,7 @@ console.log(mensaje[0]) //devuelve el caracter en la posición indicada
 
 console.log(mensaje.charAt(0)) //devuelve el caracter en la posición indicada
 
-
+console.log(mensaje.charCodeAt(0)) //devuelve el código ASCII del caracter en la posición indicada
 
 
 
