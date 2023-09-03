218da24eb3272240c89604a791d22dbabeaaf868
kaiserkey <kaiserkey2@gmail.com>
2023-09-03 20:21:18 -0300

    new changes

--- ejercicio12.js
+++ ejercicio12.js
@@ -10,4 +10,5 @@ console.log(`El texto tiene una longitud de: ${mensaje.length}`);
 //indexOf
 console.log(`La palabra Javascript empieza en la posición: ${mensaje.indexOf("Javascript")}`);
 
-
+//lastIndexOf
+console.log(`La palabra Javascript empieza en la posición: ${mensaje.lastIndexOf("Javascript")}`);
