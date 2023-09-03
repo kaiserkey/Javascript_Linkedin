1b1db010fff9a40c3d104560ad433b5b19863f9a
kaiserkey <kaiserkey2@gmail.com>
2023-09-03 20:48:34 -0300

    new changes

--- ejercicio12.js
+++ ejercicio12.js
@@ -100,3 +100,9 @@ var numero = 20;
 console.log(typeof(numero.toString())); //convierte un número a string
 
 
+var mensaje = "Ayer fui a comprar unos tenis y después fuimos a comer";
+
+console.log(mensaje.indexOf("fui"));
+console.log(mensaje.lastIndexOf("fui"));
+console.log(mensaje.search("hoy"));
+console.log(mensaje.search(/un/));
